928a6f6fc00d439cd81df8b459c2aaa93be3549c
Jeff Davis <jeff@j-davis.com>
2020-05-02 11:58:13 -0700

    Improve documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
-// emit build flags for building a plugin-style cdylib, which may have
-// undefined symbols that will be resolved when the plugin is loaded
-// into the host program
+
+//! Provides workarounds for two problems encountered with ``cdylib``
+//! crates for plugin-style shared libraries, where the library refers
+//! to symbols in the host program:
+//! 1. Linking errors on some platforms due to undefined symbols
+//! 1. Difficulty finding the produced shared library for testing or installation
+
+/// call from build.rs to emit build flags for building a plugin-style cdylib
 pub fn buildflags() {
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-cdylib-link-arg=-undefined dynamic_lookup");
@@ -9,8 +14,8 @@ pub fn buildflags() {
     }
 }
 
-// return the absolute path of the generated cdylib, using the
-// CARGO_PKG_NAME environment variable and the current directory.
+/// return the absolute path of the generated cdylib, using the
+/// CARGO_PKG_NAME environment variable and the current directory.
 pub fn cdylib_path() -> String {
     let pkgname = std::env::var("CARGO_PKG_NAME").unwrap();
     let libname = pkgname_to_libname(pkgname);
